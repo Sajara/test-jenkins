61d9488f888b3a3e4bc7d51456a0b877fc8c1b84
Sajara <ir.sajara@gmail.com>
2019-07-03 12:53:47 +0530

    iofekl

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello world how are u");   
+         System.out.println("Hello");   
       }
 } 
